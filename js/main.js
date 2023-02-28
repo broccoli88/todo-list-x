264a089c929c4c2a0c584b8e9d6777882a7f3cbd
KiAi <94138724+broccoli88@users.noreply.github.com>
2023-02-28 21:26:00 +0100

    Added part of functionality
    -creating new task
    -removing tasks

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const tasksList = document.querySelector(".tasks");
 const createTask = document.querySelector(".create-task");
 const taskInput = document.querySelector(".task-input");
 const closeInputButton = document.querySelector("#close-input");
@@ -12,7 +13,43 @@ closeInputButton.addEventListener("click", () => {
 });
 
 createTaskButton.addEventListener("click", () => {
-    const taskList = document.querySelector(".tasks");
+    const taskTitle = document.querySelector(`.input__title`);
+    const taskDescription = document.querySelector(`.input__description`);
+    const li = document.createElement("li");
+    const h3 = document.createElement("h3");
+    const p = document.createElement("p");
+    const div = document.createElement("div");
+    const buttonEdit = document.createElement("button");
+    const buttonDelete = document.createElement("button");
 
-    // check how to add new items to list => DOM manipulation
+    li.classList.add("task__item");
+    h3.classList.add("task__heading");
+    p.classList.add("task__description");
+    div.classList.add("buttons");
+    buttonEdit.classList.add(`button`, `button--outline`);
+    buttonDelete.classList.add(
+        `button`,
+        `button--outline`,
+        `button--red`,
+        `delete-button`
+    );
+
+    h3.textContent = taskTitle.value;
+    p.textContent = taskDescription.value;
+    buttonEdit.textContent = `Edit`;
+    buttonDelete.textContent = `Delete`;
+
+    div.appendChild(buttonEdit);
+    div.appendChild(buttonDelete);
+    li.appendChild(h3);
+    li.appendChild(p);
+    li.appendChild(div);
+    tasksList.appendChild(li);
+});
+
+tasksList.addEventListener(`click`, (e) => {
+    if (e.target.className.includes(`delete-button`)) {
+        const li = e.target.parentElement.parentElement;
+        tasksList.removeChild(li);
+    }
 });
